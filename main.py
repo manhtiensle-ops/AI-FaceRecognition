5628aae5ecb1e683ebab43c6c079f697fb34a97f
froundsun <johndoe@example.com>
2026-03-12 18:46:43 +0700

    ok

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import requests
 import os
 import warnings
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
@@ -9,12 +10,13 @@ from PIL import Image
 from facenet_pytorch import MTCNN, InceptionResnetV1
 from torchvision import transforms
 
+
+BASE = "http://127.0.0.1:5000"
 DETECT_EVERY = 3          
 DOWNSCALE_MAX_W = 640     
 BOX_THICKNESS = 2
 MIN_FACE = 40             
 THRESHOLDS = [0.6, 0.7, 0.7] 
-# Ngưỡng khoảng cách nhận diện (Euclidean distance). Dưới 0.8 thường là cùng một người.
 RECOGNITION_THRESHOLD = 0.8 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -62,7 +64,7 @@ for person_name in os.listdir(base_dataset_folder):
     if not os.path.isdir(person_folder):
         continue
         
-    print(f" -> Đang học khuôn mặt của: {person_name} ...")
+    print(f"Đang học khuôn mặt của: {person_name} ...")
     embeddings_list = []
     
     for filename in os.listdir(person_folder):
@@ -72,7 +74,7 @@ for person_name in os.listdir(base_dataset_folder):
             
             if ref_img is None: continue
             
-            # Lấy kích thước ảnh gốc
+            # take original image size
             H, W = ref_img.shape[:2]
             
             #DOWNSCALE 
@@ -80,36 +82,35 @@ for person_name in os.listdir(base_dataset_folder):
             if W > DOWNSCALE_MAX_W:
                 scale = DOWNSCALE_MAX_W / float(W)
                 newW, newH = int(W * scale), int(H * scale)
-                # Tạo một bản nháp thu nhỏ 
+                # resize image
                 img_small = cv2.resize(ref_img, (newW, newH), interpolation=cv2.INTER_LINEAR)
             else:
                 img_small = ref_img
                 
-            # Đổi màu cho cả bản gốc (để cắt) và bản nháp (để tìm)
+            # Change to RGB
             ref_rgb = cv2.cvtColor(ref_img, cv2.COLOR_BGR2RGB)
             rgb_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
             
-            # Bắt MTCNN tìm mặt trên BẢN NHÁP THU NHỎ
+            # use mtcnn to detect on small image
             ref_boxes, _ = mtcnn.detect(Image.fromarray(rgb_small))
             
             if ref_boxes is not None:
                 inv_scale = 1.0 / scale
                 box = ref_boxes[0]
                 
-                # --- ÁP DỤNG KỸ THUẬT NHÂN NGƯỢC ---
-                # Phóng to tọa độ từ bản nháp về lại kích thước thật
+                # Caculate real coordinate from small image
                 rx1 = box[0] * inv_scale
                 ry1 = box[1] * inv_scale
                 rx2 = box[2] * inv_scale
                 ry2 = box[3] * inv_scale
                 
-                # Ép khung không bị tràn ra ngoài viền ảnh GỐC (W, H)
+                # solving with overflow picture's border
                 clamped = clamp_box_xyxy(rx1, ry1, rx2, ry2, W, H)
                 
                 if clamped is not None:
                     cx1, cy1, cx2, cy2 = clamped
                     
-                    # Cắt mặt từ BẢN GỐC SẮC NÉT
+                    # cat face
                     face_cropped = ref_rgb[cy1:cy2, cx1:cx2]
                     
                     if face_cropped.shape[0] < 40 or face_cropped.shape[1] < 40:
@@ -133,11 +134,11 @@ if len(known_embeddings) == 0:
 
 print("\nĐã học xong tất cả! Bắt đầu mở Webcam...")
 
-# ===== 4. VÒNG LẶP XỬ LÝ WEBCAM =======#
+# ------Open WebCam -----#
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
-    print("Không mở được webcam")
+    print("Can not open webcam")
     raise SystemExit(1)
 
 last_tracked_faces = [] 
@@ -151,7 +152,7 @@ while True:
     H, W = frame.shape[:2]
 
     if frame_id % DETECT_EVERY == 0:
-        # Thu nhỏ ảnh 
+        # Resize pictures
         scale = 1.0
         if W > DOWNSCALE_MAX_W:
             scale = DOWNSCALE_MAX_W / float(W)
@@ -162,14 +163,16 @@ while True:
 
         rgb_small = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
         
-        # Gọi MTCNN tìm mặt
+        # Using mtcnn to detect faces
         boxes, _ = mtcnn.detect(Image.fromarray(rgb_small))
 
         new_tracked_faces = []
         if boxes is not None:
             inv_scale = 1.0 / scale 
+
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #Change to RGB
             for box in boxes:
-                # Phóng to tọa độ về kích thước ảnh gốc
+                # Take back real coordination 
                 x1, y1 = box[0] * inv_scale, box[1] * inv_scale
                 x2, y2 = box[2] * inv_scale, box[3] * inv_scale
                 
@@ -177,23 +180,22 @@ while True:
                 if clamped is not None:
                     cx1, cy1, cx2, cy2 = clamped
                     
-                    # CẮT KHUÔN MẶT TỪ ẢNH GỐC SẮC NÉT ĐỂ ĐƯA ĐI NHẬN DIỆN
-                    face_crop = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)[cy1:cy2, cx1:cx2]
+                    # Cut faces from RGB picture
+                    face_crop = frame_rgb[cy1:cy2, cx1:cx2]
                     
-                    # Bỏ qua nếu mặt quá mờ hoặc quá nhỏ
+                    # Ignore faces that are too blurry or small
                     if face_crop.shape[0] < 40 or face_crop.shape[1] < 40:
                         continue
 
-                    # Đưa mặt qua AI Resnet để trích xuất 512 con số
+                    # Using Resnet to take faces data
                     face_tensor = face_transform(face_crop).unsqueeze(0).to(device)
                     with torch.no_grad():
                         emb = resnet(face_tensor)[0]
                     
-                    # --- SO SÁNH VỚI NHIỀU NGƯỜI TRONG TỪ ĐIỂN ---
-                    min_dist = float('inf') # Đặt khoảng cách ban đầu là vô cực
+                    # --- Compare with other faces ---#
+                    min_dist = float('inf')
                     best_match_name = "Unknown"
                     
-                    # Lặp qua tất cả những người AI đã học để tìm người giống nhất
                     for person_name, known_emb in known_embeddings.items():
                         dist = (emb - known_emb).norm().item()
                         
@@ -201,30 +203,38 @@ while True:
                             min_dist = dist
                             best_match_name = person_name
                     
-                    # Nếu độ chênh lệch nhỏ hơn ngưỡng cho phép thì nhận diện thành công
                     if min_dist < RECOGNITION_THRESHOLD:
                         name = f"{best_match_name} ({min_dist:.2f})"
-                        color = (0, 255, 0) # Xanh lá
+                        color = (0, 255, 0) # Green
                     else:
                         name = f"Unknown ({min_dist:.2f})"
-                        color = (0, 0, 255) # Đỏ
+                        color = (0, 0, 255) # Red
 
                     x, y, w, h = xyxy_to_xywh(clamped)
                     new_tracked_faces.append((x, y, w, h, name, color))
 
+
         last_tracked_faces = new_tracked_faces
 
-    # Vẽ khung và in tên lên ảnh
+    # print frame
     for (x, y, w, h, name, color) in last_tracked_faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, BOX_THICKNESS)
         cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
 
-    cv2.imshow("Face Recognition He Thong", frame)
+    cv2.imshow("Face Recognition", frame)
 
+    # #---------------send API ----------------#
+    # payload = {
+    #     "data":f"Detect {name}"
+    # }
+    # response = requests.post(BASE + "/AI", json=payload)
+    # print(response.json())
+    # #----------------------------------------#
+    
     frame_id += 1
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q') or k == 27:
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
